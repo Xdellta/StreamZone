0b7d187ec893a5c149f62f90e44827e94b03ec52
Xdellta <66367896+Xdellta@users.noreply.github.com>
2023-03-10 20:26:10 +0100

    navigation

---
+++ src/components/AsideMenu.vue
@@ -0,0 +1,112 @@
+<script setup>
+    import LeftIcon from './icons/left.vue'
+</script>
+
+<template>
+    <div id="headline">
+        <span class="headline__title">Obserwowane kanały:</span>
+        <LeftIcon id="btn-AsideMenu" />
+    </div>
+
+    <div id="watchedChannels">
+        <div class="watchedChannels__item">
+            <img src="../assets/images/avatar.jpg" alt="Aria Sweeney" class="avatar">
+            <div class="userDescript">
+                <span class="nick">Aria Sweeney</span>
+                <p class="description">Assassin's Creed</p>
+            </div>
+            <div class="status status-online"></div>
+        </div>
+
+        <div class="watchedChannels__item">
+            <img src="../assets/images/avatar.jpg" alt="Aria Sweeney" class="avatar">
+            <div class="userDescript">
+                <span class="nick">Aria Sweeney</span>
+                <p class="description">Assassin's Creed</p>
+            </div>
+            <div class="status status-offline"></div>
+        </div>
+
+        <div class="watchedChannels__item">
+            <img src="../assets/images/avatar.jpg" alt="Aria Sweeney" class="avatar">
+            <div class="userDescript">
+                <span class="nick">Aria Sweeney</span>
+                <p class="description">Assassin's Creed</p>
+            </div>
+            <div class="status status-online"></div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    #headline {
+        width: 100%;
+        padding: 11% 0;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .headline__title {
+        color: var(--blue);
+        font-size: 17px;
+        font-weight: bold;
+    }
+
+    #btn-AsideMenu {
+        width: 35px;
+        height: 35px;
+        fill: var(--gray);
+    }
+
+    #btn-AsideMenu:hover {
+        cursor: pointer;
+        fill: var(--white);
+    }
+
+    #watchedChannels {
+        width: 100%;
+        height: 52%;
+        border-bottom: 3px solid var(--main2);
+    }
+
+    .watchedChannels__item {
+        width: 100%;
+        height: 40px;
+        margin-bottom: 20px;
+        display: grid;
+        grid-template-columns: 40px auto 12px;
+        align-items: center;
+        gap: 14px;
+    }
+
+    .avatar {
+        height: 40px;
+        width: 40px;
+        border-radius: 50%;
+    }
+
+    .userDescript {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .description {
+        color: var(--gray);
+        font-size: 14px;
+    }
+
+    .status {
+        width: 12px;
+        height: 12px;
+        border-radius: 50%;
+    }
+
+    .status-online {
+        background-color: var(--green);
+    }
+
+    .status-offline {
+        background-color: var(--red);
+    }
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,85 @@
+<script setup>
+  import LogoIcon from './icons/logo.vue'
+  import SearchIcon from './icons/search.vue'
+  import BellIcon from './icons/bell.vue'
+</script>
+
+<template>
+  <div class="logo">
+    <LogoIcon class="LogoIcon"/>
+    <h1>StreamZone</h1>
+  </div>
+
+  <div class="pagesMenu">
+    <RouterLink to="/" class="link">Przegląd</RouterLink>
+    <RouterLink to="/Streamy" class="link">Streamy</RouterLink>
+    <RouterLink to="/Streamy" class="link">Biblioteka</RouterLink>
+    <RouterLink to="/Streamy" class="link">Wydarzenia</RouterLink>
+  </div>
+
+  <div class="otherMenu">
+    <SearchIcon class="otherMenu__icon"/>
+    <BellIcon class="otherMenu__icon"/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .logo, .pagesMenu, .pagesMenu {
+    display: flex;
+    align-items: center;
+  }
+
+  .logo > h1 {
+    font-size: 23px;
+  }
+
+  .LogoIcon {
+    width: 50px;
+    height: 50px;
+  }
+
+  .pagesMenu {
+    width: 500px;
+    justify-content: space-between;
+    font-size: 19px;
+  }
+
+  .link {
+    color: var(--gray);
+    text-decoration: none;
+    padding: 5.5px 20px;
+    border-radius: 10px;
+  }
+
+  .link.router-link-active {
+    background-color: var(--blue);
+    color: var(--white);
+  }
+
+  .link:hover {
+    cursor: pointer;
+    transform: scale(1.02);
+    color: var(--white);
+  }
+
+  .otherMenu {
+    width: 90px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .otherMenu__icon {
+    width: 36px;
+    height: 36px;
+    padding: 4px;
+    border-radius: 10px;
+    fill: var(--white);
+  }
+
+  .otherMenu__icon:hover {
+    cursor: pointer;
+    transform: scale(1.03);
+    background-color: var(--main1);
+  }
+</style>
